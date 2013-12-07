14faf16138dc03dcc4788253bb4f1a47c91b5c2a
Matthias Esterl, Inventor <me@madcity.at>
2013-12-07 01:37:51 +0100

    Updated to jQuery 2.0.* and Bootstrap 3, removed custom theme

--- src/config.php
+++ src/config.php
@@ -1,12 +1,16 @@
 <?php
 
-//Enable/Disable Debug Mode
+// Enable/Disable Debug Mode
 $app['debug'] = true;
 
+// Define data-paths
 $app['wiki.path'] =  __DIR__.'/../data/wiki/';
+$app['uploads.path'] =  __DIR__.'/../data/uploads/';
 
-$app['stylesheet'] = 'bootstrap-spacelab.min.css';
+// Define Bootstrap Theme
+$app['theme'] = 'bootstrap-theme.min.css';
 
+// Setup authentication
 $app['auth'] =  false;
 $app['auth.user'] = 'username';
-$app['auth.password'] = md5( 'password' );
+$app['auth.password'] = md5('password');
