4476c06aaddfd710697f41ee7145696b44b07e1a
Matthias Esterl, Inventor <me@madcity.at>
2013-12-09 16:43:07 +0100

    Resolved jslint issues

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,54 +1,57 @@
-(function($)
-{
-	miki =
-	{
-		init: function()
-		{
+/*global navigator, location, window, document, jQuery, $*/
+
+var miki;
+(function ($) {
+	'use strict';
+
+	miki = {
+		init: function () {
 			miki.layout();
 			miki.hotkeys();
 		},
-		
-		layout: function()
-		{			
+
+		layout: function () {
 			/* Resize textarea with window. */
-			$(window).resize(function() {
+			$(window).resize(function () {
 				$('#form_content').height($(window).height() - 260);
 			}).resize();
-			
+
 			/* Tabs for edit page. */
-			$('.nav-tabs a').click(function(e) {
+			$('.nav-tabs a').click(function (e) {
 
 				var mode = this.hash.slice(1);
-				if( mode != 'editor' )
-					$('#'+mode).load('preview/'+mode, {'content': $('#form_content').val()});
-		
-				if( mode )
-					$('.nav-tabs .dropdown-toggle span').html( $(this).html() );
-			
+				if (mode !== 'editor') {
+					$('#' + mode).load('preview/' + mode, {'content': $('#form_content').val()});
+				}
+
+				if (mode) {
+					$('.nav-tabs .dropdown-toggle span').html($(this).html());
+				}
+
 				$(this).tab('show');
 				e.preventDefault();
-			})
+			});
 		},
-		hotkeys: function()
-		{
+		hotkeys: function () {
 			var key = 'ctrl';
-			if( navigator.appVersion.indexOf("Mac") != -1 )
+			if (navigator.appVersion.indexOf("Mac") !== -1) {
 				key = 'meta';
-			
+			}
+
 			/* Default hotkeys (Save, Cancel, Edit, Home)*/
-			$('#wmd-input').bind('keydown.'+key+'_s',function(){ $('form').submit(); return false; });
-			$(document).bind('keydown.alt_'+key+'_x',function(){ location.href = $('#form_cancel').attr('href'); return false; });
-			$(document).bind('keydown.'+key+'_e',function(){ location.href = $('#page_edit').attr('href'); return false; });
-			$(document).bind('keydown.alt_'+key+'_h',function(){ location.href = $('#menu_home').attr('href'); return false; });
-			
-			/* ToDo-List Hotkeys */
-			$('.container li').click(function(e){
-				if( e.altKey )
-				{
-					if($(this).children('s').length)
+			$('#wmd-input').bind('keydown.' + key + '_s', function () { $('form').submit(); return false; });
+			$(document).bind('keydown.alt_' + key + '_x', function () { location.href = $('#form_cancel').attr('href'); return false; });
+			$(document).bind('keydown.' + key + '_e', function () { location.href = $('#page_edit').attr('href'); return false; });
+			$(document).bind('keydown.alt_' + key + '_h', function () { location.href = $('#menu_home').attr('href'); return false; });
+
+			/* T0D0-List Hotkeys */
+			$('.container li').click(function (e) {
+				if (e.altKey) {
+					if ($(this).children('s').length) {
 						$(this).children('s').contents().unwrap();
-					else
+					} else {
 						$(this).wrapInner('<s />');
+					}
 
 					//add '@done (2012-09-12 12:22)'?
 					//save !?
@@ -57,7 +60,11 @@
 				}
 			});
 		}
-	}
-})(jQuery);
+	};
+}(jQuery));
 
-$(document).ready(function (){ miki.init() });
+$(document).ready(function () {
+	'use strict';
+
+	miki.init();
+});
